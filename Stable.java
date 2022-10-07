f10038b8d2a498fe5bfdde2f17f3479dc236a3ae
Victor Rodriguez <112190314+VictorRodriguez7@users.noreply.github.com>
2022-10-07 01:49:42 +0000

    everything

--- Stable.java
+++ Stable.java
@@ -1,1 +1,14 @@
+import java.util.ArrayList;
+
+public class Stable {
+    String address = "123 Sesame Street";
+
+    ArrayList<Horse> horseArrayList = new ArrayList<Horse>();
+
+    Stable(String address){
+        this.address = address;
+    }
+
+
+}
 //Create the Stable class here.
